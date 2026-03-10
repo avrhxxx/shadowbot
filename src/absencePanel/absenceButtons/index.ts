bb8715715ab0052242ae942302b5cd679ee2fe5e
avrhxxx <stelczyk0797@gmail.com>
2026-03-10 14:03:03 +0100

    Create index.ts

---
+++ src/absencePanel/absenceButtons/index.ts
@@ -0,0 +1,6 @@
+// src/absencePanel/absenceButtons/index.ts
+
+export * from "./absenceAdd";
+export * from "./absenceRemove";
+export * from "./absenceShowList";
+export * from "./absenceSettings";
