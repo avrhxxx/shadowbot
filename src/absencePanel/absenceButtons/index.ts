7c96d101b2726089ea4191fc790a317b5304f0d3
avrhxxx <stelczyk0797@gmail.com>
2026-03-11 10:43:23 +0100

    Update index.ts

--- src/absencePanel/absenceButtons/index.ts
+++ src/absencePanel/absenceButtons/index.ts
@@ -2,4 +2,10 @@ export { handleAddAbsence, handleAddAbsenceSubmit } from "./absenceAdd";
 export { handleRemoveAbsence, handleRemoveAbsenceSubmit } from "./absenceRemove";
 export { handleAbsenceList } from "./absenceList";
 export { handleSettings, handleSettingsSelect } from "./absenceSettings";
-export { notifyAbsenceAdded, notifyAbsenceRemoved, notifyAbsenceAutoClean, initAbsenceNotifications, startAbsenceAutoCleaner } from "./absenceNotification";
+export { 
+  notifyAbsenceAdded, 
+  notifyAbsenceRemoved, 
+  notifyAbsenceAutoClean, 
+  initAbsenceNotifications, 
+  startAbsenceAutoCleaner 
+} from "./absenceNotification";
