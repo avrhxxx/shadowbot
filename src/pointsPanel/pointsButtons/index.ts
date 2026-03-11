f760974dad73b2e86913f3ddf62d549d098f7d6f
avrhxxx <stelczyk0797@gmail.com>
2026-03-11 20:10:01 +0100

    Create index.ts

---
+++ src/pointsPanel/pointsButtons/index.ts
@@ -0,0 +1,6 @@
+// src/pointsPanel/pointsButtons/index.ts
+
+export * as PBCreate from "./pointsCreate";
+export * as PBListWeeks from "./pointsListWeeks";
+export * as PBDonations from "./pointsDonations";
+export * as PBDuel from "./pointsDuel";
