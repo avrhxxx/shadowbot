2121d137c8e577c639800babf974888cea2f319d
avrhxxx <stelczyk0797@gmail.com>
2026-03-11 02:29:40 +0100

    Update index.ts

--- src/absencePanel/absenceButtons/index.ts
+++ src/absencePanel/absenceButtons/index.ts
@@ -1,4 +1,6 @@
+// src/absencePanel/index.ts
 export { handleAddAbsence, handleAddAbsenceSubmit } from "./absenceAdd";
 export { handleRemoveAbsence, handleRemoveAbsenceSubmit } from "./absenceRemove";
-export { handleAbsenceList } from "./absenceList"; // <- zmiana
+export { handleAbsenceList } from "./absenceList";
 export { handleSettings, handleSettingsSelect } from "./absenceSettings";
+export { updateAbsenceNotifications, initAbsenceNotifications, startAbsenceAutoCleaner } from "./absenceNotification";
