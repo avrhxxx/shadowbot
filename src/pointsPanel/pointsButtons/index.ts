857127b90839cb8413f832a6ee6978abb2beb9df
avrhxxx <stelczyk0797@gmail.com>
2026-03-12 12:52:35 +0100

    Update index.ts

--- src/pointsPanel/pointsButtons/index.ts
+++ src/pointsPanel/pointsButtons/index.ts
@@ -4,3 +4,8 @@ export * as pointsCreate from "./pointsCreate";
 export * as pointsListWeeks from "./pointsListWeeks";
 export * as pointsDonations from "./pointsDonations";
 export * as pointsDuel from "./pointsDuel";
+export * as pointsAdd from "./pointsAdd";
+export * as pointsRemove from "./pointsRemove";
+export * as pointsCompare from "./pointsCompare";
+export * as pointsList from "./pointsList";
+export * as pointsSelectWeek from "./pointsSelectWeek";
