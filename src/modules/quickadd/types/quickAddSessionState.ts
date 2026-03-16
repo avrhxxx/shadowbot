4258b065718adb96c8ec47deb9672ec2c4321966
avrhxxx <stelczyk0797@gmail.com>
2026-03-16 15:56:23 +0100

    Delete quickAddSessionState.ts

--- src/modules/quickadd/types/quickAddSessionState.ts
+++
@@ -1,9 +0,0 @@
-// Stany sesji QuickAdd
-export enum QuickAddSessionState {
-  INIT = 'INIT',                 // sesja dopiero startuje
-  COLLECTING_DATA = 'COLLECTING_DATA', // dane wprowadzane
-  PREVIEW_READY = 'PREVIEW_READY',     // bufor gotowy do podglądu
-  CONFIRMED = 'CONFIRMED',       // moderator zatwierdził dane
-  CANCELLED = 'CANCELLED',       // sesja anulowana
-  TIMEOUT = 'TIMEOUT'            // sesja zakończona timeoutem
-}
