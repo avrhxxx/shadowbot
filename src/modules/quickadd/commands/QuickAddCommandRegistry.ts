795f672d55b742d1a816a8817333a7688c1fd4ba
avrhxxx <stelczyk0797@gmail.com>
2026-03-16 22:55:10 +0100

    Create QuickAddCommandRegistry.ts

---
+++ src/modules/quickadd/commands/QuickAddCommandRegistry.ts
@@ -0,0 +1,10 @@
+// src/modules/quickadd/commands/QuickAddCommandRegistry.ts
+import { Client } from "discord.js";
+
+/**
+ * Placeholder do rejestracji komend QuickAdd.
+ * Na razie loguje tylko, że komendy zostały zarejestrowane.
+ */
+export function registerQuickAddCommands(client: Client) {
+  console.log("[QuickAdd] Placeholder: QuickAdd commands registered.");
+}
