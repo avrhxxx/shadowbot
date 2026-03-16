96cdbc9a8631424e7a179aabf58e84fda9e67aa2
avrhxxx <stelczyk0797@gmail.com>
2026-03-16 16:58:49 +0100

    Create index.ts

---
+++ src/modules/quickadd/ocr/index.ts
@@ -0,0 +1,8 @@
+// src/modules/quickadd/ocr/index.ts
+// Placeholder indeks dla OCR
+
+export * from "./ImagePreprocessor";
+export * from "./OCRRunner";
+export * from "./OCRService";
+export * from "./RowCropper";
+export * from "./RowDetector";
