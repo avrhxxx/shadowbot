5e82f4a4089f1c698f10152b539deaa7db8a5a4b
avrhxxx <stelczyk0797@gmail.com>
2026-03-16 16:57:14 +0100

    Create index.ts

---
+++ src/modules/quickadd/index.ts
@@ -0,0 +1,13 @@
+// src/modules/quickadd/index.ts
+
+// 🔹 Placeholderowy indeks QuickAdd
+// Na razie tylko eksportujemy podfoldery, później będziemy dopisywać konkretne klasy, funkcje i komendy
+
+export * as commands from "./commands";
+export * as ocr from "./ocr";
+export * as parsers from "./parsers";
+export * as preview from "./preview";
+export * as services from "./services";
+export * as session from "./session";
+export * as types from "./types";
+export * as utils from "./utils";
