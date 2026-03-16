ca3d317406f2a2814d41c1f28327a7be3db394fd
avrhxxx <stelczyk0797@gmail.com>
2026-03-16 16:58:30 +0100

    Create index.ts

---
+++ src/modules/quickadd/commands/index.ts
@@ -0,0 +1,16 @@
+// src/modules/quickadd/commands/index.ts
+// Placeholder indeks dla wszystkich komend QuickAdd
+
+export * from "./adjust";
+export * from "./cadd";
+export * from "./cattend";
+export * from "./confirm";
+export * from "./dnadd";
+export * from "./dpadd";
+export * from "./preview";
+export * from "./redo";
+export * from "./repair";
+export * from "./rradd";
+export * from "./rrattend";
+export * from "./QuickAddChannelInit";
+export * from "./QuickAddCommandRegistry";
