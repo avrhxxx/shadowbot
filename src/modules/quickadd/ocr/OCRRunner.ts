5a09b3226791239543e4be5c52d0bb13b628aff1
avrhxxx <stelczyk0797@gmail.com>
2026-03-17 09:40:18 +0100

    Create OCRRunner.ts

---
+++ src/modules/quickadd/ocr/OCRRunner.ts
@@ -0,0 +1,8 @@
+import { OCRResult } from "../types/OCRResult";
+
+export class OCRRunner {
+  static async runOCR(imageBuffer: Buffer): Promise<OCRResult[]> {
+    // 🔹 placeholder: na razie zwraca pustą tablicę
+    return [];
+  }
+}
