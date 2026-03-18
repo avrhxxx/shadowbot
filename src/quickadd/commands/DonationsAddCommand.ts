d1562d2aaf842b7b72b5bc2fd799ae1a4897c295
avrhxxx <stelczyk0797@gmail.com>
2026-03-18 17:13:32 +0100

    Update DonationsAddCommand.ts

--- src/quickadd/commands/DonationsAddCommand.ts
+++ src/quickadd/commands/DonationsAddCommand.ts
@@ -1,20 +1,5 @@
-import { CommandInteraction } from "discord.js";
-import { startQuickAddSession } from "./startQuickAddSession";
+import { Message } from "discord.js";
 
-export async function dnadd(interaction: CommandInteraction) {
-  const weekArg = interaction.options.getString("week");
-
-  if (!weekArg || !/^\d{8}$/.test(weekArg)) {
-    await interaction.reply({
-      content: "❌ Format: MMDDMMDD (np. 01030703).",
-      ephemeral: true
-    });
-    return;
-  }
-
-  await startQuickAddSession({
-    interaction,
-    eventType: "DN",
-    week: weekArg
-  });
+export async function dnadd(message: Message) {
+  await message.reply("⚠️ DN jeszcze nie działa (naprawiamy system)");
 }
