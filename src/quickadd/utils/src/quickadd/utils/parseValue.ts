d6adb91344cc13e01595481f45f746a75aa65aac
avrhxxx <stelczyk0797@gmail.com>
2026-03-18 18:20:25 +0100

    Create parseValue.ts

---
+++ src/quickadd/utils/src/quickadd/utils/parseValue.ts
@@ -0,0 +1,21 @@
+export function parseValue(input: string): number | null {
+  if (!input) return null;
+
+  let value = input.toLowerCase().replace(",", ".");
+
+  let multiplier = 1;
+
+  if (value.endsWith("k")) {
+    multiplier = 1_000;
+    value = value.slice(0, -1);
+  } else if (value.endsWith("m")) {
+    multiplier = 1_000_000;
+    value = value.slice(0, -1);
+  }
+
+  const num = Number(value);
+
+  if (isNaN(num)) return null;
+
+  return Math.round(num * multiplier);
+}
