e1ae4f2c02098cc14813dbfad6f3f59a30b623ef
avrhxxx <stelczyk0797@gmail.com>
2026-03-18 15:19:24 +0100

    Delete src/modules/quickadd directory

--- src/modules/quickadd/ocr/OCRRunner.ts
+++
@@ -1,8 +0,0 @@
-import { OCRResult } from "../types/OCRResult";
-
-export class OCRRunner {
-  static async runOCR(imageBuffer: Buffer): Promise<OCRResult[]> {
-    // 🔹 placeholder: na razie zwraca pustą tablicę
-    return [];
-  }
-}
--- src/modules/quickadd/types/QuickAddSessionState.ts
+++
@@ -1,2 +0,0 @@
-// src/modules/quickadd/types/QuickAddSessionState.ts
-export type QuickAddSessionState = "INIT" | "COLLECTING_DATA" | "PREVIEW_READY" | "CONFIRMED" | "CANCELLED" | "TIMEOUT";
