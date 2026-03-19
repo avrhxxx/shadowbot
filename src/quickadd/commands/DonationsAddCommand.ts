4b523425edeaa09a9669b739dc4059edfaa89162
avrhxxx <stelczyk0797@gmail.com>
2026-03-19 12:03:25 +0100

    Delete src/quickadd/commands/DonationsAddCommand.ts

--- src/quickadd/commands/DonationsAddCommand.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from "discord.js";
-import { startQuickAddSession } from "../utils/startQuickAddSession";
-
-export async function dnadd(message: Message) {
-  await startQuickAddSession(message, "dn", "add");
-}
