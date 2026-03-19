cfa97fd22b750fb910c0745a003531e492288688
avrhxxx <stelczyk0797@gmail.com>
2026-03-19 12:03:13 +0100

    Delete src/quickadd/commands/ReservoirAddCommand.ts

--- src/quickadd/commands/ReservoirAddCommand.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from "discord.js";
-import { startQuickAddSession } from "../utils/startQuickAddSession";
-
-export async function rradd(message: Message) {
-  await startQuickAddSession(message, "rr", "add");
-}
