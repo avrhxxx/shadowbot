db9be071c4392945de4280d579ba13beca850cb2
avrhxxx <stelczyk0797@gmail.com>
2026-03-19 12:02:47 +0100

    Delete src/quickadd/commands/ReservoirAttendCommand.ts

--- src/quickadd/commands/ReservoirAttendCommand.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from "discord.js";
-import { startQuickAddSession } from "../utils/startQuickAddSession";
-
-export async function rrattend(message: Message) {
-  await startQuickAddSession(message, "rr", "attend");
-}
