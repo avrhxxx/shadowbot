7058ae47fd4f2b033e3f3a842380246b5369a87a
avrhxxx <stelczyk0797@gmail.com>
2026-03-19 12:25:03 +0100

    Update StartCommand.ts

--- src/quickadd/commands/StartCommand.ts
+++ src/quickadd/commands/StartCommand.ts
@@ -1,16 +1,6 @@
 import { Message } from "discord.js";
-import { SessionManager } from "../session/SessionManager";
+import { startQuickAddSession } from "../utils/startQuickAddSession";
 
 export async function start(message: Message) {
-  const guildId = message.guildId!;
-
-  SessionManager.createSession(guildId, {
-    moderatorId: message.author.id,
-    channelId: message.channel.id,
-    mode: "auto", // 🔥 nowy tryb
-  });
-
-  await message.reply(
-    "🟢 Auto session started.\n📸 Send screenshots — bot will detect everything automatically."
-  );
+  await startQuickAddSession(message);
 }
