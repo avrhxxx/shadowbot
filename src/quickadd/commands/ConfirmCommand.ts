a319961477e462c2cb09d64b8b23cbe7c047f570
avrhxxx <stelczyk0797@gmail.com>
2026-03-20 11:55:50 +0100

    Update ConfirmCommand.ts

--- src/quickadd/commands/ConfirmCommand.ts
+++ src/quickadd/commands/ConfirmCommand.ts
@@ -1,3 +1,5 @@
+// src/quickadd/commands/ConfirmCommand.ts
+
 import { Message } from "discord.js";
 import { SessionManager } from "../session/SessionManager";
 import { SessionData } from "../session/SessionData";
