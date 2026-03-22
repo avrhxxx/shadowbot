2b7d0b8b4dd95a1d1bfbb99a7a66d688fe47a973
avrhxxx <stelczyk0797@gmail.com>
2026-03-22 21:23:14 +0100

    Delete src/quickadd directory

--- src/quickadd/commands/StartCommand.ts
+++
@@ -1,7 +0,0 @@
-// src/quickadd/commands/StartCommand.ts
-import { Message } from "discord.js";
-import { startQuickAddSession } from "../utils/startQuickAddSession";
-
-export async function start(message: Message) {
-  await startQuickAddSession(message);
-}
