d3bbad45986d4e080667c353382c803fab510420
avrhxxx <stelczyk0797@gmail.com>
2026-03-22 21:52:08 +0100

    Delete QuickAddListener.ts

--- rc/quickadd/QuickAddListener.ts
+++
@@ -1,9 +0,0 @@
-// src/quickadd/QuickAddListener.ts
-
-import { Client } from "discord.js";
-
-export function registerQuickAddListener(client: Client) {
-  client.on("messageCreate", async (message) => {
-    // 🔥 placeholder
-  });
-}
