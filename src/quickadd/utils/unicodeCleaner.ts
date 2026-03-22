c35580716eb5eb589787593efdf46a1a44cd1f19
avrhxxx <stelczyk0797@gmail.com>
2026-03-22 10:35:06 +0100

    Delete unicodeCleaner.ts

--- src/quickadd/utils/unicodeCleaner.ts
+++
@@ -1,7 +0,0 @@
-// src/quickadd/utils/unicodeCleaner.ts
-export function unicodeCleaner(input: string): string {
-  return input
-    .normalize("NFC")
-    .trim()
-    .replace(/\p{C}/gu, ""); // usuwa niewidoczne znaki Unicode
-}
