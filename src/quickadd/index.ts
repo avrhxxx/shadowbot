5786e39ff30b0f64b2011f8418feb9372dfaa9a3
avrhxxx <stelczyk0797@gmail.com>
2026-03-22 21:36:18 +0100

    Create index.ts

---
+++ src/quickadd/index.ts
@@ -0,0 +1,26 @@
+// src/quickadd/index.ts
+
+// Publiczny entry do QuickAdd
+
+export * from "./QuickAddListener";
+
+// core
+export * from "./core/QuickAddPipeline";
+export * from "./core/QuickAddSession";
+export * from "./core/QuickAddTypes";
+
+// OCR
+export * from "./ocr/OCRService";
+
+// parsing
+export * from "./parsing";
+
+// detection
+export * from "./detection/ImageTypeDetector";
+
+// mapping
+export * from "./mapping/NicknameResolver";
+
+// integrations
+export * from "./integrations/EventsIntegration";
+export * from "./integrations/PointsIntegration";
