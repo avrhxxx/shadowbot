df709da49d2b79532e00c84b602afd0db2db02fe
avrhxxx <stelczyk0797@gmail.com>
2026-03-24 09:49:18 +0100

    Update index.ts

--- src/quickadd/commands/commands/index.ts
+++ src/quickadd/commands/commands/index.ts
@@ -6,3 +6,4 @@ export { previewCommand } from "./preview/preview";
 export { startCommand } from "./start/start";
 export { endCommand } from "./end/end";
 export { adjustCommand } from "./adjust/adjust";
+export { confirmCommand } from "./confirm/confirm"; // ✅ ADDED
