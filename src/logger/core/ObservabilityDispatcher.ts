6b65ba672be26398dd75d674e7dd54e5c3c540d2
avrhxxx <stelczyk0797@gmail.com>
2026-03-28 10:33:12 +0100

    Update ObservabilityDispatcher.ts

--- src/logger/core/ObservabilityDispatcher.ts
+++ src/logger/core/ObservabilityDispatcher.ts
@@ -2,19 +2,25 @@
 // 📁 src/logger/core/ObservabilityDispatcher.ts
 // =====================================
 
-import { LogInput } from "../observability/ObservabilityTypes";
+import { LoggerEmitOptions } from "../observability/ObservabilityTypes";
 import { ObservabilityEngine } from "./ObservabilityEngine";
+
+/**
+ * ❄️ FROZEN MODULE
+ * Dispatch layer for logs.
+ */
 
 // =====================================
 // 🔥 DISPATCHER
 // =====================================
 
 export const ObservabilityDispatcher = {
-  emit(input: LogInput) {
-    // 🔮 FUTURE:
+  emit(input: LoggerEmitOptions) {
+    // 🔮 FUTURE EXTENSIONS:
     // - send to DB
     // - send to Sentry
     // - send to queue
+    // - send to external observability tools
 
     ObservabilityEngine.emit(input);
   },
