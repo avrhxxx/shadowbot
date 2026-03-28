fa8a4e273ec47e27fe092288746ecbb2728d7efd
avrhxxx <stelczyk0797@gmail.com>
2026-03-28 12:00:31 +0100

    Create index.ts

---
+++ src/system/points/index.ts
@@ -0,0 +1,6 @@
+// =====================================
+// 📁 src/system/points/index.ts
+// =====================================
+
+export { handlePointsInteraction } from "../../pointsPanel/pointsHandler";
+export * from "../../pointsPanel/pointsService";
