13186a72a261d41936e646b7977dd74df4824eba
avrhxxx <stelczyk0797@gmail.com>
2026-03-28 11:11:07 +0100

    Delete src/observability directory

--- src/observability/runtime/Time.ts
+++
@@ -1,7 +0,0 @@
-// =====================================
-// 📁 src/observability/runtime/Time.ts
-// =====================================
-
-export function getTime(): string {
-  return new Date().toISOString().split("T")[1].split(".")[0];
-}
