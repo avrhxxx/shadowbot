5d331bf820f121e4affdb0f926d65a0266b03ebc
avrhxxx <stelczyk0797@gmail.com>
2026-03-28 11:03:19 +0100

    Create PointsIds.ts

---
+++ src/pointsPanel/ids/PointsIds.ts
@@ -0,0 +1,24 @@
+// =====================================
+// 📁 src/pointsPanel/ids/PointsIds.ts
+// =====================================
+
+/**
+ * 🧾 PLACEHOLDER
+ */
+
+// import {
+//   createTraceId,
+//   resolveDisplayId,
+// } from "../../ids/IdGenerator";
+
+// const PREFIX = "pt";
+
+// export const PointsIds = {
+//   trace() {
+//     return `${PREFIX}-${createTraceId()}`;
+//   },
+
+//   display(realId: string) {
+//     return resolveDisplayId(realId);
+//   },
+// };
