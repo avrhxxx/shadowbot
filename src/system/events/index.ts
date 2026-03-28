b9b16645ddd5be6dfc8de39ac61603be106f42bf
avrhxxx <stelczyk0797@gmail.com>
2026-03-28 11:59:43 +0100

    Create index.ts

---
+++ src/system/events/index.ts
@@ -0,0 +1,7 @@
+// =====================================
+// 📁 src/system/events/index.ts
+// =====================================
+
+export { handleEventInteraction } from "../../eventsPanel/eventHandlers";
+export { initEventReminders } from "../../eventsPanel/eventsButtons/eventsReminder";
+export * from "../../eventsPanel/eventService";
