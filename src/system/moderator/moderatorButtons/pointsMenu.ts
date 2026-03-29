cdabe234d2595e9a1c21cc8710935f75908b7ec5
avrhxxx <stelczyk0797@gmail.com>
2026-03-29 11:33:26 +0200

    Update pointsMenu.ts

--- src/system/moderator/moderatorButtons/pointsMenu.ts
+++ src/system/moderator/moderatorButtons/pointsMenu.ts
@@ -1,14 +1,30 @@
+// src/system/moderator/moderatorButtons/pointsMenu.ts
+
 import { Interaction } from "discord.js";
 import { renderPointsPanel } from "../../pointsPanel/pointsPanel";
+import { logger } from "../../../core/logger/log";
 
-export async function handlePointsMenu(interaction: Interaction) {
+export async function handlePointsMenu(
+  interaction: Interaction,
+  traceId: string
+) {
   if (!interaction.isButton()) return;
 
   const panel = renderPointsPanel();
 
+  logger.emit({
+    scope: "moderator.buttons",
+    event: "moderator_points_menu_open",
+    traceId,
+    context: {
+      guildId: interaction.guildId,
+      id: interaction.customId,
+    },
+  });
+
   await interaction.reply({
     content: panel.content,
     components: panel.components,
-    ephemeral: true // tylko Ty widzisz panel
+    ephemeral: true
   });
 }
