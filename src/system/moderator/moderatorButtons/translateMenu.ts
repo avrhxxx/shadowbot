dfd760de1cf8bae892fc43a9b4d63a7380430e86
avrhxxx <stelczyk0797@gmail.com>
2026-03-29 20:46:08 +0200

    Update translateMenu.ts

--- src/system/moderator/moderatorButtons/translateMenu.ts
+++ src/system/moderator/moderatorButtons/translateMenu.ts
@@ -1,22 +1,20 @@
 // src/system/moderator/moderatorButtons/translateMenu.ts
 
 import { Interaction } from "discord.js";
-import { logger } from "../../../core/logger/log";
+import { log } from "../../../core/logger/log";
+import { TraceContext } from "../../../core/trace/TraceContext";
 
 export async function handleTranslateMenu(
   interaction: Interaction,
-  traceId: string
+  ctx: TraceContext
 ) {
   if (!interaction.isButton()) return;
 
-  logger.emit({
-    scope: "moderator.buttons",
-    event: "moderator_translate_menu_open",
-    traceId,
-    context: {
-      guildId: interaction.guildId,
-      id: interaction.customId,
-    },
+  const l = log.ctx(ctx);
+
+  l.event("moderator_translate_menu_open", {
+    guildId: interaction.guildId,
+    id: interaction.customId,
   });
 
   await interaction.reply({
