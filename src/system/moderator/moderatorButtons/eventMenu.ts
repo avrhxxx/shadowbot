1d0364265e35c08517551bfbcf62f36ae325586b
avrhxxx <stelczyk0797@gmail.com>
2026-03-29 20:42:16 +0200

    Update eventMenu.ts

--- src/system/moderator/moderatorButtons/eventMenu.ts
+++ src/system/moderator/moderatorButtons/eventMenu.ts
@@ -2,24 +2,22 @@
 
 import { Interaction } from "discord.js";
 import { renderEventPanel } from "../../eventsPanel/eventPanel";
-import { logger } from "../../../core/logger/log";
+import { log } from "../../../core/logger/log";
+import { TraceContext } from "../../../core/trace/TraceContext";
 
 export async function handleEventMenu(
   interaction: Interaction,
-  traceId: string
+  ctx: TraceContext
 ) {
   if (!interaction.isButton()) return;
 
+  const l = log.ctx(ctx);
+
   const panel = renderEventPanel();
 
-  logger.emit({
-    scope: "moderator.buttons",
-    event: "moderator_event_menu_open",
-    traceId,
-    context: {
-      guildId: interaction.guildId,
-      id: interaction.customId,
-    },
+  l.event("moderator_event_menu_open", {
+    guildId: interaction.guildId,
+    id: interaction.customId,
   });
 
   await interaction.reply({
