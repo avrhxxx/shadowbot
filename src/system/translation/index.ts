8432893f0fd9e56cb01c56f406d084b854b797f2
avrhxxx <stelczyk0797@gmail.com>
2026-03-30 21:13:14 +0200

    Update index.ts

--- src/system/translation/index.ts
+++ src/system/translation/index.ts
@@ -2,12 +2,4 @@
 // 📁 src/system/translation/index.ts
 // =====================================
 
-/**
- * 🚪 Public API for Translation system
- */
-
-// =============================
-// 🔹 INIT
-// =============================
-
-export { initTranslationModule } from "./TranslationModule";
+export { initTranslationListener } from "./translationListener";
