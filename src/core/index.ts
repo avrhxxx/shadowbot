53cc3bea5eaa892689f46fb105ac5ad4b5d5bbf8
avrhxxx <stelczyk0797@gmail.com>
2026-03-30 13:31:03 +0200

    Create index.ts

---
+++ src/core/index.ts
@@ -0,0 +1,8 @@
+// =====================================
+// 📁 src/core/index.ts
+// =====================================
+
+export * from "./ids/IdGenerator";
+export * from "./trace/TraceContext";
+export * from "./logger/log";
+export * from "./router/systemRouter";
