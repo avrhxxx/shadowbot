0766d0952641d2683ac7c4ff5b6c1319cd9eade9
avrhxxx <stelczyk0797@gmail.com>
2026-04-01 11:18:29 +0200

    Create discordClient.ts

---
+++ src/integrations/discord/discordClient.ts
@@ -0,0 +1,15 @@
+import { Client } from "discord.js";
+
+let client: Client | null = null;
+
+export function setDiscordClient(c: Client) {
+  client = c;
+}
+
+export function getDiscordClient(): Client {
+  if (!client) {
+    throw new Error("Discord client not initialized");
+  }
+
+  return client;
+}
