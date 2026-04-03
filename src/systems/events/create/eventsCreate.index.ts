01829bb504497fbe86861c24e21287e8458ed9a6
avrhxxx <stelczyk0797@gmail.com>
2026-04-03 15:23:11 +0200

    Update eventsCreate.index.ts

--- src/systems/events/create/eventsCreate.index.ts
+++ src/systems/events/create/eventsCreate.index.ts
@@ -3,19 +3,29 @@
 // =====================================
 
 import { registerEventsCreateActions } from "./eventsCreate.action";
+
+// ----------------------------
+// IMPORT STEPOW
+// ----------------------------
 import { registerSelectDayStep } from "./steps/selectDay.step";
 import { registerSelectTimeStep } from "./steps/selectTime.step";
 import { registerSubmitEventStep } from "./steps/submitEvent.step";
+import { registerBirthdayFormStep } from "./steps/birthdayForm.step";
+import { registerCustomEventFormStep } from "./steps/customEventForm.step";
+import { registerConfirmEventStep } from "./steps/confirmEvent.step";
 
 // ----------------------------
 // INIT CREATE FEATURE
 // ----------------------------
 export function initEventsCreateFeature() {
-  // Akcja startowa: Create Event
+  // 🔹 Akcja startowa: Create Event
   registerEventsCreateActions();
 
-  // Steps
+  // 🔹 Rejestracja wszystkich stepów feature
   registerSelectDayStep();
   registerSelectTimeStep();
   registerSubmitEventStep();
+  registerBirthdayFormStep();
+  registerCustomEventFormStep();
+  registerConfirmEventStep();
 }
