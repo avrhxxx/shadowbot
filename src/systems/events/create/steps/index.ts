c159eac2a46759689df1081026b746e5beb2f4a2
avrhxxx <stelczyk0797@gmail.com>
2026-04-04 04:31:56 +0200

    Create index.ts

---
+++ src/systems/events/create/steps/index.ts
@@ -0,0 +1,12 @@
+// =====================================
+// 📁 src/systems/events/create/steps/index.ts
+// =====================================
+
+// 🔹 Agregacja wszystkich stepów dla Events Create Feature
+
+export { registerSelectDayStep } from "./selectDay.step";
+export { registerSelectTimeStep } from "./selectTime.step";
+export { registerSubmitEventStep } from "./submitEvent.step";
+export { registerBirthdayFormStep } from "./birthdayForm.step";
+export { registerCustomEventFormStep } from "./customEventForm.step";
+export { registerConfirmEventStep } from "./confirmEvent.step";
