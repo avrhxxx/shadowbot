39ce35d623422a33547316721a2bc6958b6b05f0
avrhxxx <stelczyk0797@gmail.com>
2026-04-04 11:12:57 +0200

    Update devpanel.command.ts

--- src/systems/devpanel/commands/devpanel.command.ts
+++ src/systems/devpanel/commands/devpanel.command.ts
@@ -2,25 +2,19 @@
 // 📁 src/systems/devpanel/commands/devpanel.command.ts
 // =====================================
 
-import {
-  ChatInputCommandInteraction,
-  CacheType,
-} from "discord.js";
-
+import { ChatInputCommandInteraction, CacheType } from "discord.js";
 import { devpanelMainView } from "../views/devpanel.view";
+import { renderView } from "@/core/ui/uiEngine";
 
 // =====================================
 // 🚀 EXECUTE
 // =====================================
 
-export async function handleDevpanelCommand(
-  interaction: ChatInputCommandInteraction<CacheType>
-) {
-  const view = await devpanelMainView();
-
+export async function handleDevpanelCommand(interaction: ChatInputCommandInteraction<CacheType>) {
+  const view = await renderView(interaction, devpanelMainView.id);
   await interaction.reply({
     content: view.content,
-    components: view.components,
+    components: view.buttons,
     ephemeral: true,
   });
 }
