16b341da283aa342cda7a281bc20040343bf1d56
avrhxxx <stelczyk0797@gmail.com>
2026-04-04 04:56:08 +0200

    Update index.ts

--- src/systems/events/create/steps/index.ts
+++ src/systems/events/create/steps/index.ts
@@ -2,11 +2,18 @@
 // 📁 src/systems/events/create/steps/index.ts
 // =====================================
 
-// 🔹 Agregacja wszystkich stepów dla Events Create Feature
-
+// 🔹 Eksport wszystkich kroków (Steps)
 export { registerSelectDayStep } from "./selectDay.step";
 export { registerSelectTimeStep } from "./selectTime.step";
 export { registerSubmitEventStep } from "./submitEvent.step";
 export { registerBirthdayFormStep } from "./birthdayForm.step";
 export { registerCustomEventFormStep } from "./customEventForm.step";
 export { registerConfirmEventStep } from "./confirmEvent.step";
+
+// 🔹 Eksport wszystkich widoków (Views)
+export { selectDayView } from "./selectDay.step";
+export { selectTimeView } from "./selectTime.step";
+export { submitEventView } from "./submitEvent.step";
+export { birthdayFormView } from "./birthdayForm.step";
+export { customEventFormView } from "./customEventForm.step";
+export { confirmEventView } from "./confirmEvent.step";
