d964131dc5c7862c2b100b17cee9e841fcd5c5b8
avrhxxx <stelczyk0797@gmail.com>
2026-04-04 16:40:52 +0200

    Create index.ts

---
+++ src/ui/api/index.ts
@@ -0,0 +1,9 @@
+// =====================================
+// 📁 src/ui/api/index.ts
+// =====================================
+
+// 🔹 Publiczne API dla devów
+export * as view from "./view";
+export * as button from "./button";
+export * as modal from "./modal";
+export * as interaction from "./interaction";
