ddedcdedf5d7cfd3bccb91df668580024251c5e3
avrhxxx <stelczyk0797@gmail.com>
2026-04-04 03:32:02 +0200

    Update eventsCreate.action.ts

--- src/systems/events/create/eventsCreate.action.ts
+++ src/systems/events/create/eventsCreate.action.ts
@@ -1,13 +1,12 @@
-// src/systems/events/create/eventsCreate.action.ts
-
 import { registerUIAction } from "@/ui/core/uiRouter";
 import { eventsCreateView } from "./eventsCreate.view";
 
 export function registerEventsCreateActions() {
   registerUIAction("events.create.start", {
     system: "events",
-    handler: async () => {
-      return eventsCreateView();
+    handler: async (interaction) => {
+      // Wyświetlamy widok eventów
+      await interaction.update(eventsCreateView());
     },
   });
 }
